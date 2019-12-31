78558566f6fdcedb4cc26876064ae9e972d1e8cd
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-12-31 08:13:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length <= array.length[0]
     return array.join("")
-  elsif array.length >= array.length[1]
+  elsif array.length >= array.length[2]
     return array.join(" and ")
   else 
     array.join(", ")
