f8e061263d6f79debd20d8ff038c20d08a5222f1
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-12-31 08:27:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   while array.length == 1
     return array.join("")
   while array.length == 2
-    return array.join(" and ")
+    return "#{array.join(" and ")}"
   while array.length == 3
     array.join(", ")
 end
